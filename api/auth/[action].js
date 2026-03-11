015d4f8d6e5f76dbded274c7da9a42cf2960a5b4
Silentboy07A <sakthibalamurugan06@gmail.com>
2026-03-11 15:34:47 +0530

    fix: ignore /api in SPA fallback rewrite in vercel.json to resolve 405 error

--- api/auth/[action].js
+++ api/auth/[action].js
@@ -1,4 +1,11 @@
 module.exports = async (req, res) => {
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        return res.status(200).end();
+    }
+
     const { action } = req.query;
 
     switch (action) {
